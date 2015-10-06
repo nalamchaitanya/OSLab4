73c6ac20852c7645c38c54cff947a62ac7bd4607
Nalam V S S Krishna Chaitanya <cs13b021@smail.iitm.ac.in>
2015-10-06 13:34:53 +0530

    Basic shell frame work is added.

---
+++ shell.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+	char *prompt;
+	char *line;
+	// TODO write the prompt function in the .h and .c files.
+	while(1)
+	{
+		prompt = getPrompt();
+		// should be done before every loop because command may change cwd.
+		printf("%s $",prompt);
+		// TODO take the line from the input stream and process it.
+		getline(&line,);
+		// TODO do the processing of the input and exec the code.
+		execute(line);
+	}
+}
